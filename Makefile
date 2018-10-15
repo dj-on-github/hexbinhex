b1c22ec5746aa757d05b8ba3af2bd94e410b21af
root <root@blacksheepatorencomail.com>
2018-10-15 15:29:27 -0700

    Adding bin201. A program to turn binary to ascii binary

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ CFLAGS = -I/usr/local/include -m64 -g -Wall
 LDFLAGS = -L/usr/local/lib 
 LDLIBS = -lm
 
-all: hex2bin bin2hex bin2nistoddball
+all: hex2bin bin2hex bin201 bin2nistoddball
 
 bin2nistoddball: bin2nistoddball.c
 	$(CC) $(CFLAGS) $(LDFLAGS) bin2nistoddball.c  -o bin2nistoddball $(LDLIBS)
@@ -14,13 +14,18 @@ hex2bin: hex2bin.c
 bin2hex: bin2hex.c
 	$(CC) $(CFLAGS) $(LDFLAGS) bin2hex.c  -o bin2hex $(LDLIBS)
 
+bin201: bin201.c
+	$(CC) $(CFLAGS) $(LDFLAGS) bin201.c  -o bin201 $(LDLIBS)
+
 install:
 	cp bin2hex /usr/local/bin
+	cp bin201  /usr/local/bin
 	cp hex2bin /usr/local/bin
 	cp bin2nistoddball /usr/local/bin
 
 clean:
 	rm bin2hex
+	rm bin201
 	rm hex2bin
 	rm bin2nistoddball
 
---
+++ bin201.c
@@ -0,0 +1,233 @@
+
+/*
+    bin2hex - A utility to convert binary data to hex data.
+    
+    Copyright (C) 2017  David Johnston
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+    -----
+    
+    Contact. David Johnston dj@deadhat.com
+*/
+/* make isnan() visible */
+#define _DEFAULT_SOURCE 
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <getopt.h>
+
+void display_usage() {
+fprintf(stderr,"Usage: bin201 [-w <width>][-h][-o <out filename>] [filename]\n");
+fprintf(stderr,"\n");
+fprintf(stderr,"Convert binary data to ascii binary (01001001).\n");
+fprintf(stderr,"  Author: David Johnston, dj@deadhat.com\n");
+fprintf(stderr,"\n");
+}
+
+void printsample(unsigned char *thesample)
+{
+    int tempindex;
+    int j;
+    int i;
+   tempindex = 0;
+    for (j=0;j<16;j++)
+    {
+            for (i=0;i<16;i++) printf("%02X",thesample[tempindex++]);
+            printf("\n");
+    }
+}
+
+/********
+* main() is mostly about parsing and qualifying the command line options.
+*/
+
+int main(int argc, char** argv)
+{
+    int opt;
+	int i;
+    int j;
+	
+	FILE *ifp;
+	FILE *ofp;
+	int using_outfile;
+	int using_infile;
+	char filename[1000];
+	char infilename[1000];
+	
+    int width;
+    int bigendian;    
+    int abyte;
+
+	/* Defaults */
+	using_outfile = 0;       /* use stdout instead of outputfile*/
+
+    width = 32;
+    bigendian = 0;
+
+    filename[0] = (char)0;
+	infilename[0] = (char)0;
+
+	/* get the options and arguments */
+    int longIndex;
+
+    char optString[] = "o:k:w:bh";
+    static const struct option longOpts[] = {
+    { "output", no_argument, NULL, 'o' },
+    { "width", required_argument, NULL, 'w' },
+    { "bigendian", no_argument, NULL, 'b' },
+    { "help", no_argument, NULL, 'h' },
+    { NULL, no_argument, NULL, 0 }
+    };
+
+    opt = getopt_long( argc, argv, optString, longOpts, &longIndex );
+    while( opt != -1 ) {
+        switch( opt ) {
+            case 'o':
+                using_outfile = 1;
+                strcpy(filename,optarg);
+                break;
+            case 'w':
+                width=atoi(optarg);
+                if (width<1) width=32;
+                break;
+            case 'f':
+                using_infile = 1;
+                strcpy(infilename,optarg);
+                break;
+            case 'b':
+                bigendian = 1;
+                break;
+                
+            case 'h':   /* fall-through is intentional */
+            case '?':
+                display_usage();
+                exit(0);
+                 
+            default:
+                /* You won't actually get here. */
+                break;
+        }
+         
+        opt = getopt_long( argc, argv, optString, longOpts, &longIndex );
+    } // end while
+    
+    if (optind < argc) {
+        strcpy(infilename,argv[optind]);
+        using_infile = 1;
+    }
+
+	/* Range check the var args */
+
+
+	/* open the output file if needed */
+
+	if (using_outfile==1)
+	{
+		ofp = fopen(filename, "w");
+		if (ofp == NULL) {
+			perror("failed to open output file for writing");
+			exit(1);
+		}
+	}
+
+	/* open the input file if needed */
+	if (using_infile==1)
+	{
+        ifp =  fopen(infilename, "rb");
+		    
+		if (ifp == NULL) {
+			perror("failed to open input file for reading");
+			exit(1);
+		}
+	}
+
+    unsigned char buffer[2048];
+    unsigned char outbuffer[10000];
+    int outindex = 0;
+    int bitcount = 0;
+    int done=0;
+    size_t len;
+    int lastcharnl = 0;
+    char binch;
+
+    do {
+        if (using_infile==1)
+            len = fread(buffer, 1, 512 , ifp);
+        else
+            len = fread(buffer, 1, 512 , stdin);
+            
+        if (len == 0) break;
+        
+        for (i=0;i<len;i++) {
+            abyte = buffer[i];
+            for (j=0;j<8;j++) {
+                if (bigendian == 1) {
+                    if (((abyte >> j) & 0x01) == 0) {
+                        binch = '0';
+                    } else {
+                        binch = '1';
+                    }
+                } else {
+                    if (((abyte >> (7-j)) & 0x01) == 0) {
+                        binch = '0';
+                    } else {
+                        binch = '1';
+                    }
+                }
+                bitcount++;
+
+                sprintf((char *)&((outbuffer[outindex])),"%c",binch);  
+                outindex+=1;
+                    
+                if (bitcount==width) {
+                    sprintf((char *)&(outbuffer[outindex]),"\n");
+                    outindex++;
+                    bitcount=0;
+                    lastcharnl = 1;
+                }
+                else {
+                    lastcharnl = 0;
+                }
+            }
+        }
+        
+        if (using_outfile)
+            fwrite(outbuffer, outindex,1,ofp);
+        else
+            fwrite(outbuffer, outindex,1,stdout);
+        
+        outindex = 0;
+      
+        
+    } while (done==0);
+    
+    if (lastcharnl==0) {
+        if (using_outfile)
+            fprintf(ofp,"\n");
+        else
+            printf("\n");
+    }
+    
+    if (using_outfile==1) fclose(ofp);
+
+    return 0;    
+}
+
+
